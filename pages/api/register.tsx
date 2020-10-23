d74b0cff2d4ad7127fafb30d443134bed2e191b6
Mau Reyes <Mauricio.Reyes@rsmus.com>
2020-10-23 17:12:25 -0600

    ﻿[feat]: Adding register endpoint
    -- to commit
    * Registration of the user with hashed pasword

--- pages/api/register.tsx
+++ pages/api/register.tsx
@@ -1,6 +1,7 @@
+import { hash } from 'bcryptjs';
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-import { dbConnect } from '../../utils/dbConnection';
 import { User } from '../../models/User';
+import { dbConnect } from '../../utils/dbConnection';
 
 interface NextApiRequestWithRegister extends NextApiRequest {
   body: {
@@ -13,26 +14,37 @@ const register: NextApiHandler = async (
   req: NextApiRequestWithRegister,
   res: NextApiResponse
 ) => {
-  let db = await dbConnect();
-
-  // Creating the user
-  try {
-    let user = await new User({
-      email: 'reyes.marq@gmail.com',
-      password: 'testing',
-    }).save();
-
-    console.log(user);
-
-    return res.status(201).json({ success: true, data: user });
-  } catch (error) {
-    return res.status(500).json({
-      sucess: false,
-      error: 'Internal server error' + error.message,
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({
+      message: `Method ${req.method} Not allowed`,
     });
-  }
+  } else {
+    try {
+      await dbConnect();
+      let { email, password } = req.body;
 
-  res.status(200).json({ msg: 'ok' });
+      // validate if there is an existing user with that email
+      let existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(403).json({
+          success: false,
+          error: 'Email already in used',
+        });
+      }
+
+      let hashPassword = await hash(password, 10);
+      let user = await new User({ email, password: hashPassword }).save();
+
+      return res.status(201).json({ success: true, data: user });
+    } catch (error) {
+      return res.status(500).json({
+        sucess: false,
+        error: 'Internal server error' + error.message,
+      });
+    }
+  }
 };
 
 export default register;
