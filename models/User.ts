013bb3e6aeea01195d7e880595e411d74ee224e4
Mau Reyes <Mauricio.Reyes@rsmus.com>
2020-10-23 18:52:50 -0600

    migrating to typescript native

--- models/User.ts
+++ models/User.ts
@@ -1,20 +1,19 @@
-import { models } from 'mongoose';
-import { createSchema, Type, typedModel } from 'ts-mongoose';
+// https://hackernoon.com/how-to-link-mongoose-and-typescript-for-a-single-source-of-truth-94o3uqc?source=rss
+import { Document, model, Schema } from 'mongoose';
 
-export interface IUser {
+interface IUser {
   email: string;
   password: string;
 }
 
-let userschema = createSchema(
-  {
-    email: Type.string({ required: true, unique: true, index: true }),
-    password: Type.string({ required: true }),
-  },
-  { timestamps: true }
-);
+interface UserModel extends IUser, Document {}
 
-let User = models.User || typedModel('User', userschema);
+let userSchema = new Schema<IUser>({
+  email: { type: String, required: true, index: true },
+  password: { type: String, required: true },
+});
+
+let User = model<UserModel>('User', userSchema);
 
 export { User };
 
