a133e31bdda92e4dd9011b0462a62fdea248b9b5
Mau Reyes <Mauricio.Reyes@rsmus.com>
2020-10-26 23:13:07 -0600

    creating the pages for login and register

---
+++ pages/login.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const login: React.FC<Props> = () => {
+  return <div>login</div>;
+};
+
+export default login;
---
+++ pages/register.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const register: React.FC<Props> = () => {
+  return <div>register</div>;
+};
+
+export default register;
