d50cebc3d7355ae558a99a81ff135d5a37bd414c
Mau Reyes <Mauricio.Reyes@rsmus.com>
2020-10-26 23:23:16 -0600

    submitted form markup

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 interface Props {}
 
 const register: React.FC<Props> = () => {
-  return <div>register</div>;
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log('form submitted');
+        console.log('email value', email);
+        console.log('password value', password);
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          value={email}
+          placeholder="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
 };
 
 export default register;
